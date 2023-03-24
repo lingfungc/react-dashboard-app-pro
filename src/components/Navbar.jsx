35f9ecfdb5d07c3c772266e11ec9c51aa16b691e
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-24 12:31:26 +0000

    added car chat notifications user-profile btn in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,6 +49,35 @@ const Navbar = () => {
           color="blue"
           icon={<FiShoppingCart />}
         />
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img src={avatar} alt="avatar" className="rounded-full w-8 h-8" />
+            <p>
+              <span className="text-gray-400 tet-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
       </div>
     </div>
   );
