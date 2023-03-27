3fdd820586e1263edae2af3464daea5d5b6716d7
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-27 16:46:06 +0100

    added stacked-bar-chart in revenue-section

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -17,8 +17,34 @@ import {
   stackedPrimaryYAxis,
 } from "../../data/dummy";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+const Stacked = ({ width, height }) => {
+  const legendSettings = {
+    visible: true,
+    position: "Top",
+    background: "white",
+    textStyle: { fontFamily: "Archivo" },
+  };
+
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={legendSettings}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
