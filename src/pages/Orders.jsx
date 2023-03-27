c2d2aff6e4f36a9fdaa468eaef2958b576ce947a
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-27 23:44:17 +0100

    added orders data

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -15,13 +15,20 @@ import {
   Inject,
 } from "@syncfusion/ej2-react-grids";
 
-import { orderData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent id="grid-comp" dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   );
 };
