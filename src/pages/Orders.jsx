2efeece489aa355c1ea04a82b2a29494be55e701
Ling Fung Chan <justinlfchan@gmail.com>
2023-03-27 23:40:12 +0100

    added imports in order.jsx and header.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { orderData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
+  );
 };
 
 export default Orders;
