6b56184b7bf2fabd982038ebe974c05f98a24e76
Ling Fung Chan <justinlfchan@gmail.com>
2023-04-05 14:31:32 +0100

    fixed sparkline-component bug when re-sizing the browser screen

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ const App = () => {
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
-                <Route path="/color-picker" element={<ColorPicker />} />
+                <Route path="/color picker" element={<ColorPicker />} />
 
                 {/* Charts */}
                 <Route path="/line" element={<Line />} />
@@ -123,7 +123,7 @@ const App = () => {
                 <Route path="/bar" element={<Bar />} />
                 <Route path="/pie" element={<Pie />} />
                 <Route path="/financial" element={<Financial />} />
-                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/color mapping" element={<ColorMapping />} />
                 <Route path="/pyramid" element={<Pyramid />} />
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
