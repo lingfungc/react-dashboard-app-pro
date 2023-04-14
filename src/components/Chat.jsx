c6f5825f6eb57f41c1da1a9f1c7ea12f13225131
Ling Fung Chan <justinlfchan@gmail.com>
2023-04-14 21:43:26 +0100

    completed chat popup message box

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -8,14 +8,14 @@ const Chat = () => {
   const { currentColor, handleClickClose } = useStateContext();
 
   return (
-    <div className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
+    <div
+      id="chat"
+      className="nav-item absolute right-5 md:right-52 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96"
+    >
       <div className="flex justify-between items-center">
         <div className="flex gap-3">
           <p className="font-semibold text-lg dark:text-gray-200">Messages</p>
-          <button
-            type="button"
-            className="text-white text-xs p-1 px-2 bg-orange"
-          >
+          <button type="button" className="text-xs p-1 px-2 bg-orange">
             5 New
           </button>
         </div>
@@ -28,6 +28,52 @@ const Chat = () => {
         >
           <MdOutlineCancel />
         </button>
+      </div>
+
+      <div className="mt-5">
+        {chatData?.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 border-b-1 border-color p-3 leading-8 cursor-pointer"
+          >
+            <div className="relative">
+              <img
+                className="rounded-full h-10 w-10"
+                src={item.image}
+                alt={item.message}
+              />
+              <span
+                style={{ background: currentColor }}
+                className="absolute inline-flex rounded-full h-2 w-2 right-0 top-1"
+              />
+            </div>
+
+            <div>
+              <p className="font-semibold dark:text-gray-200">{item.message}</p>
+              <p className="text-gray-500 dark:text-gray-400 text-sm">
+                {item.desc}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400 text-xs">
+                {item.time}
+              </p>
+            </div>
+          </div>
+        ))}
+
+        <div className="mt-5">
+          <button
+            type="button"
+            style={{
+              backgroundColor: currentColor,
+              color: "white",
+              borderRadius: "10px",
+            }}
+            className="p-3 hover:drop-shadow-xl hover:bg-light-gray w-full"
+            onClick={() => handleClickClose("chat")}
+          >
+            See all messages
+          </button>
+        </div>
       </div>
     </div>
   );
