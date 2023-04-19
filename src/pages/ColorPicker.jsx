dbaa49c01f7066a89e952263dad218d3617ce3cf
Ling Fung Chan <justinlfchan@gmail.com>
2023-04-19 12:59:46 +0100

    updated color-picker pen img background color for dark mode

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { useEffect, React } from "react";
 
 import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 
 import { Header } from "../components";
+
+import { useStateContext } from "../contexts/ContextProvider";
 
 // * Change the color of the pen accordingly
 const change = (args) => {
@@ -11,18 +13,22 @@ const change = (args) => {
 };
 
 const ColorPicker = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <div
       id="calendar"
-      className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"
+      className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl"
     >
       <Header category="App" title="Color Picker" />
       <div className="text-center">
-        <div id="preview" />
+        <div id={currentMode === "Light" ? "preview-light" : "preview-dark"} />
 
         <div className="flex justify-center items-center gap-20 flex-wrap">
           <div>
-            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <p className="text-2xl font-semibold mt-2 mb-4 dark:text-gray-200">
+              Inline Pallete
+            </p>
             <ColorPickerComponent
               id="inline-pallete"
               mode="Palette"
@@ -34,7 +40,9 @@ const ColorPicker = () => {
           </div>
 
           <div>
-            <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+            <p className="text-2xl font-semibold mt-2 mb-4 dark:text-gray-200">
+              Inline Picker
+            </p>
             <ColorPickerComponent
               id="inline-pallete"
               mode="Picker"
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -13,7 +13,7 @@ const Kanban = () => {
   return (
     <div
       id="kanban"
-      className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"
+      className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl"
     >
       <Header category="App" title="Kanban" />
       <KanbanComponent
