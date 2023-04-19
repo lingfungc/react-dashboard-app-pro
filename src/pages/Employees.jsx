f31d43d5e7bcf60ebac213f3e898274837b55c2d
Ling Fung Chan <justinlfchan@gmail.com>
2023-04-19 11:12:27 +0100

    updated employees grid-table-row color theme for dark mode

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, React } from "react";
 
 import {
   GridComponent,
@@ -14,11 +14,36 @@ import {
 import { employeesData, employeesGrid } from "../data/dummy";
 import { Header } from "../components";
 
+import { useStateContext } from "../contexts/ContextProvider";
+
 const Employees = () => {
+  const { currentMode } = useStateContext();
+
+  useEffect(() => {
+    const gridTable = document.getElementById("grid-comp_content_table");
+    const svgPathElements = document.querySelectorAll(".e-row svg path");
+
+    if (currentMode === "Light") {
+      gridTable.style.backgroundColor = "white";
+      gridTable.style.color = "black";
+
+      svgPathElements.forEach((e) => {
+        e.setAttribute("stroke", "#000");
+      });
+    } else {
+      gridTable.style.backgroundColor = "rgb(51 55 62)";
+      gridTable.style.color = "white";
+
+      svgPathElements.forEach((e) => {
+        e.setAttribute("stroke", "#fff");
+      });
+    }
+  });
+
   return (
     <div
       id="employees-grid"
-      className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl"
+      className="m-2 md:m-10 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl"
     >
       <Header category="Page" title="Employees" />
       <GridComponent
